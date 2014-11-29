ea0fa85ec89f8ebd26166cb0ad7036a34f731832
Kennedy <Kennedy@Kennedys-MacBook-Pro.local>
2014-11-29 00:17:49 -0500

    init commit

---
+++ _assets/javascripts/plugins.js
@@ -0,0 +1,9 @@
+//= require vendor/ss-standard
+//= require vendor/ss-social
+
+// http://mailto.ninja/
+
+
+$(document).ready(function(){
+
+});
---
+++ _assets/javascripts/script.js
@@ -0,0 +1,12 @@
+//= require plugins
+
+$(document).ready(function(){
+
+
+	// Navigation for mobile
+	$('.menu').click(function(e){
+		e.preventDefault();
+		$('nav').slideToggle();
+	});
+
+});
