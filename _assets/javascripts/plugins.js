0a7207b1c6236c8186a98ed510235291f65096db
kennedy <kennedysgarage@gmail.com>
2015-03-29 20:59:51 -0400

    adding some more content to the rental page that people commonly request

--- _assets/javascripts/plugins.js
+++ _assets/javascripts/plugins.js
@@ -1,9 +1,26 @@
 //= require vendor/ss-standard
 //= require vendor/ss-social
+//= require vendor/flexslider
 
 // http://mailto.ninja/
 
 
 $(document).ready(function(){
+
+    // Flexslider
+    $(window).load(function() {
+      $('.flexslider').flexslider();
+    });
+    
+    // http://codepen.io/cssjockey/pen/jGzuK
+  	$('ul.tabs li').click(function(){
+  		var tab_id = $(this).attr('data-tab');
+
+  		$('ul.tabs li').removeClass('current');
+  		$('.tab-content').removeClass('current');
+
+  		$(this).addClass('current');
+  		$("#"+tab_id).addClass('current');
+  	})
 
 });
